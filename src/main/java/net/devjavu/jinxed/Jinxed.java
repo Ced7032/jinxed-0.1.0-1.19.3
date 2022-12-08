d3756271598fe513b89874e679214faed6312528
Ced7032 <88561769+Ced7032@users.noreply.github.com>
2022-12-08 02:14:28 +0100

    Initial commit

---
+++ src/main/java/net/devjavu/jinxed/Jinxed.java
@@ -0,0 +1,14 @@
+package net.devjavu.jinxed;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Jinxed implements ModInitializer {
+	public static final String MOD_ID = "jinxed";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/devjavu/jinxed/JinxedClient.java
@@ -0,0 +1,10 @@
+package net.devjavu.jinxed;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class JinxedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
